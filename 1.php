f05a0571f8f632d2db309f9e49d0660379bd5a50
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2024-01-31 07:18:18 +0100

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,95 +1,117 @@
 <?php
-class ksiazka{
-        public $tytul;
-        public $autor;
-        public $rok_wydania;
-        public $wydawnictwo;
-        public $gatunek;
-        public $identyfikator;//?
+class Ksiazka {
+    protected $tytul;
+    protected $autor;
+    protected $rokWydania;
+    protected $wydawnictwo;
+    protected $gatunek;
+    private static $identyfikator = 0;
 
-        protected function wypiszTytul($tytul2){
-           $this -> $tytul = $tytul2;
-        }
-        
-        protected function wypiszAutora($autor2){
-            $this -> $autor = $autor2;
-         }
-         
-         protected function wypiszRokWydania($rw){
-            $this -> $rok_wydania = $rw;
-         }
-         
-         protected function wypiszWydawnictwo($w2){
-            $this -> $wydawnictwo = $w2;
-         }
-         
-         protected function wypiszGatunek($gatunek2){
-            $this -> $gatunek = $gatunek2;
-         }
-         
-         protected function wypiszIdentyfikator($id){
-            $this -> $identyfikator = $id;
-         }
-        public function pobierzTytul(){
-            return $this -> $tytul;
-         }
-         public function pobierzAutora(){
-            return $this -> $autor;
-         }
-         public function pobierzRokWydania(){
-            return $this -> $rok_wydania;
-         }
-         public function pobierzWydawnictwo(){
-            return $this -> $wydawnictwo;
-         }
-         public function pobierzgatunek(){
-            return $this -> $gatunek;
-         }
-         public function pobierzid(){
-            return $this -> $identyfikator;
-         }
-
-         public function __contstruct(){
-            $this -> tytul = "Pan Tadeusz";
-            $this -> autor = "Adam Mickiewicz";
-            $this -> rok_wydania = 1834;
-            $this -> wydawnictwo = "Wilga";
-            $this ->gatunek = "epos narodowy";
-            $this -> id= 1;
-         }
-}
-
-class czasopismo extends ksiazka{
-    $nr;
-    function WypiszNumer($nr2){
-        $this-> $nr =$nr2;
-    }
-    function pobierzNumer(){
-        return $this-> $nr;
+    public function __construct(s) {
+        $this->tytul = "Pan Tadeusz";
+        $this->autor = "Adam Mickiewicz";
+        $this->rokWydania = 1834;
+        $this->wydawnictwo = "GREG";
+        $this->gatunek = "Epos";
+        self::$identyfikator++;
     }
 
+    public function wpiszTytul($tytul) {
+        $this->tytul = $tytul;
+    }
+
+    public function wpiszAutora($autor) {
+        $this->autor = $autor;
+    }
+
+    public function wpiszRokWydania($rokWydania) {
+        $this->rokWydania = $rokWydania;
+    }
+
+    public function wpiszWydawnictwo($wydawnictwo) {
+        $this->wydawnictwo = $wydawnictwo;
+    }
+
+    public function wpiszGatunek($gatunek) {
+        $this->gatunek = $gatunek;
+    }
+
+    public function pobierzTytul() {
+        return $this->tytul;
+    }
+
+    public function pobierzAutora() {
+        return $this->autor;
+    }
+
+    public function pobierzRokWydania() {
+        return $this->rokWydania;
+    }
+
+    public function pobierzWydawnictwo() {
+        return $this->wydawnictwo;
+    }
+
+    public function pobierzGatunek() {
+        return $this->gatunek;
+    }
+
+    public function pobierzIdentyfikator() {
+        return self::$identyfikator;
+    }
 }
-$ksiazka1= new ksiazka();
-$ksiazka2=new ksiazka();
-$ksiazka3=new ksiazka();
 
-$ksiazka2 -> pobierzTytul("Komputer Świat. Gimp Krok po kroku");
-$ksiazka2 -> pobierzAutora("Rafał Kamiński");
-$ksiazka2 -> pobierzRokWydania(2012);
-$ksiazka2 -> pobierzWydawnictwo("Ringier Axel Springer Polska");
-$ksiazka2 -> pobierzgatunek("czasopismo");
-$ksiazka2 ->pobierzid(2);
+class Czasopismo extends Ksiazka {
+    private $nr;
 
-$ksiazka3 -> pobierzTytul("Igrzyska Śmierci");
-$ksiazka3 -> pobierzAutora("Suzanne Collins");
-$ksiazka3 -> pobierzRokWydania(2012);
-$ksiazka3 -> pobierzWydawnictwo("Media rodzina");
-$ksiazka3 -> pobierzgatunek("Literatura młodzieżowa");
-$ksiazka3 ->pobierzid(3);
+    public function wpiszNr($nr) {
+        $this->nr = $nr;
+    }
 
-$czasopismo1= new czasopismo();
-$czasopismo1= pobierzAutora("xyz");
-$czasopismo1= pobierzNumer(12/34);
+    public function pobierzNr() {
+        return $this->nr;
+    }
+}
+
+class Multibook extends Czasopismo {
+    public function __construct() {
+        parent::__construct();
+        $this->autor = "Nie podano Autora multibooka";
+    }
+}
+$ksiazka1 = new ksiazka();
+$ksiazka2 = new ksiazka();
+$ksiazka2 -> wpiszTytul("Komputer Świat - Zostań Mistrzem! Photoshop");
+$ksiazka2 -> wpiszAutora("Alicja Żebruń");
+$ksiazka2 -> wpiszRokWydania(2022);
+$ksiazka2 -> wpiszWydawnictwo("empik");
+$ksiazka2 -> wpiszGatunek("Czasopismo");
+
+$ksiazka3 = new ksiazka();
+$ksiazka2 -> wpiszTytul("Quo Vadis");
+$ksiazka2 -> wpiszAutora("Henryk Sienkiewicz");
+$ksiazka2 -> wpiszRokWydania(1896);
+$ksiazka2 -> wpiszWydawnictwo("GREG");
+$ksiazka2 -> wpiszGatunek("Powieść historyczna");
+
+
+
+
+$czasopismo1 = new czasopismo();
+$czasopismo1 -> wpiszNr("25/88");
+$czasopismo1 -> wpiszTytul("Komputer Świat - Zostań Mistrzem! Photoshop");
+$czasopismo1 -> wpiszRokWydania(2022);
+$czasopismo1 -> wpiszWydawnictwo("Empik");
+
+
+
+echo "ksiazka1 Tytuł: ". $ksiazka1->pobierzTytul()." Autor: " . $ksiazka1->pobierzAutora()." Rok Wydania: " . $ksiazka1->pobierzRokWydania()." Wydawnictwo: " . $ksiazka1->pobierzWydawnictwo() ." Gatunek: " .$ksiazka1->pobierzGatunek()."Identyfikator: " . $ksiazka1->pobierzIdentyfikator();
+echo "<br><br>";
+echo "ksiazka2 Tytuł: ". $ksiazka2->pobierzTytul()." Autor: " . $ksiazka2->pobierzAutora()." Rok Wydania: " . $ksiazka2->pobierzRokWydania()." Wydawnictwo: " . $ksiazka2->pobierzWydawnictwo() ." Gatunek: " .$ksiazka2->pobierzGatunek()."Identyfikator: " . $ksiazka2->pobierzIdentyfikator();
+echo "<br><br>";
+echo "ksiazka3 Tytuł: ". $ksiazka3->pobierzTytul()." Autor: " . $ksiazka3->pobierzAutora()." Rok Wydania: " . $ksiazka3->pobierzRokWydania()." Wydawnictwo: " . $ksiazka3->pobierzWydawnictwo() ." Gatunek: " .$ksiazka3->pobierzGatunek()."Identyfikator: " . $ksiazka3->pobierzIdentyfikator();
+echo "<br><br>";
+echo "Czasopismo 1 Tytuł: ". $czasopismo1->pobierzTytul()." Autor: " . $czasopismo1->pobierzAutora()." Rok Wydania: " . $czasopismo1->pobierzRokWydania()." Wydawnictwo: " . $czasopismo1->pobierzWydawnictwo() ." Gatunek: " .$czasopismo1->pobierzGatunek()."Identyfikator: " . $czasopismo1->pobierzIdentyfikator()."Numer: ".$czasopismo1->pobierzNr();
 
 
 
